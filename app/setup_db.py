ede9984bbf4a781ca5993b157c6c0fd56ffa7145
Ishant-codebit <ishantyadav243@gmail.com>
2026-05-08 11:24:34 +0530

    Fix pandas import inside setup function

--- app/setup_db.py
+++ app/setup_db.py
@@ -1,9 +1,9 @@
 import os
 import duckdb
-import pandas as pd
 import pyarrow
 
-def setup():
+def setup(): 
+    import pandas as pd
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATA_DIR = os.path.join(BASE_DIR, 'data')
     DB_PATH  = os.path.join(DATA_DIR, 'ecommerce.db')
